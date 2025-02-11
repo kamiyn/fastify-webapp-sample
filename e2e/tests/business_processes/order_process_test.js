f4ac2921b830502d679ba06861e4d7c1326c2fc5
Hiroyasu Tanabe <h@cosat.com>
2025-02-11 20:26:31 +0900

    pause() でインタラクティブシェルを起動する

--- e2e/tests/business_processes/order_process_test.js
+++ e2e/tests/business_processes/order_process_test.js
@@ -1,12 +1,14 @@
-SuiteOf('スモークテスト'); // Feature は Scenario をまとめたもの
+SuiteOf('注文プロセスのテスト'); // Feature は Scenario をまとめたもの
 
 // Scenario は1つのテストケース
 
-Scenario('Webサイトを開きログインする', ({ I }) => {
+Scenario('ログインし、お弁当を注文し、お弁当を受け取る', ({ I }) => {
     I.amOnPage('/')
     I.click('ログインする')
     I.fillField('ユーザー名', 'user1')
     I.fillField('パスワード', 'super-strong-passphrase')
     I.click('ログイン')
     I.see('user1 さん')
+
+    pause()
 })
