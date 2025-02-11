9f38b97a44f4624ec9de18af95e7281d2f2dd699
Hiroyasu Tanabe <h@cosat.com>
2025-02-11 21:06:04 +0900

    use now

--- e2e/tests/business_processes/order_process_test.js
+++ e2e/tests/business_processes/order_process_test.js
@@ -2,7 +2,8 @@ SuiteOf('注文プロセスのテスト'); // Feature は Scenario をまとめ�
 
 // Scenario は1つのテストケース
 
-Scenario('ログインし、お弁当を注文し、お弁当を受け取る', async ({ I }) => {
+Scenario('ログインし、お弁当を注文し、お弁当を受け取る', async ({ I, utils }) => {
+    const now = utils.now
     I.amOnPage('/')
     I.click('ログインする')
     I.fillField('ユーザー名', 'user1')
@@ -14,8 +15,8 @@ Scenario('ログインし、お弁当を注文し、お弁当を受け取る', a
     I.click('カートに入れる')
     I.fillField('お名前（受取時に必要です）', 'ユーザー1')
     I.fillField('電話番号（連絡時に必要です）', '09000000000')
-    I.fillField('受け取り日', '2025/08/01')
-    I.fillField('受け取り目安時間', '12:00AM')
+    I.fillField('受け取り日', now.format('YYYY/MM/DD'))
+    I.fillField('受け取り目安時間', now.add(1, 'hour').format('hh:mmA'))
     I.click('注文を確定する')
     const orderNo = await I.grabTextFrom('h3')
 
