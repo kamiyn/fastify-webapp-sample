4cdadb4b44839e4fb5ed1ec9eff3daa0c441f94d
Hiroyasu Tanabe <h@cosat.com>
2025-02-11 16:25:23 +0900

    ログインのスモークテストケースを追加

--- e2e/smoke_test.js
+++ e2e/smoke_test.js
@@ -1,7 +1,12 @@
 SuiteOf('スモークテスト'); // Feature は Scenario をまとめたもの
 
 // Scenario は1つのテストケース
-Scenario('example.comにアクセスする', ({ I }) => {
-    I.amOnPage('https://example.com')
-    I.see('Example Domain')
-});
+
+Scenario('Webサイトを開きログインする', ({ I }) => {
+    I.amOnPage('https://fastify-webapp-sample-production-5624.up.railway.app/')
+    I.click('ログインする')
+    I.fillField('ユーザー名', 'user1')
+    I.fillField('パスワード', 'super-strong-passphrase')
+    I.click('ログイン')
+    I.see('user1 さん')
+})
